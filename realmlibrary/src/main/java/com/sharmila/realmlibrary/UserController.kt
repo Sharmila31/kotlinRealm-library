57c28a8aeb93f77e6422ce94aad2f11efa2c5ddd
stssharmilak <sharmila.k@dotnetethics.com>
2021-08-16 14:03:28 +0530

    commit

--- realmlibrary/src/main/java/com/sharmila/realmlibrary/UserController.kt
+++ realmlibrary/src/main/java/com/sharmila/realmlibrary/UserController.kt
@@ -3,10 +3,10 @@ package com.sharmila.realmlibrary
 import android.content.Context
 import android.widget.Toast
 
-class UserController {
+public class UserController {
 
     companion object {
-        fun showToast(ctx: Context, msg: String) {
+        public fun showToast(ctx: Context, msg: String) {
 
             Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show()
         }
