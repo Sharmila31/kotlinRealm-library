b858a74153d3ec46757e9b6c887abef038d43fb3
stssharmilak <sharmila.k@dotnetethics.com>
2021-08-16 13:32:19 +0530

    Initial Commit

---
+++ realmlibrary/src/main/java/com/sharmila/realmlibrary/UserController.kt
@@ -0,0 +1,12 @@
+package com.sharmila.realmlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class UserController {
+
+    public fun showToast(ctx: Context, msg: String) {
+
+        Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show()
+    }
+}
